e17d756b84613f9f272b0d791987942fb7a7c2f6
Raquel Stankey <brv1114@yahoo.com>
2022-07-27 17:16:29 -0500

    pushing

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,45 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-let Sequelize;
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    process.env.DB_Name,
-    process.env.DB_User,
+    process.env.DB_NAME,
+    process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: "localost",
+      host: "localhost",
       dialect: "mysql",
       port: 3306,
     }
   );
 }
+
+module.exports = sequelize;
+
+// const Sequelize = require("sequelize");
+
+// require("dotenv").config();
+
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_Name,
+//     process.env.DB_User,
+//     process.env.DB_PW,
+//     {
+//       host: "localost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
 
 // //create connection to our db
 // const sequelize = new Sequelize(
